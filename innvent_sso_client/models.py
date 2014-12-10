563799cee09415a94fd825e463a79d3dd7e4a9cc
Bernardo Fontes <bernardoxhc@gmail.com>
2014-12-10 18:21:11 -0200

    Sobreescreve model manager para ter método encapsulando lógica de criação

--- innvent_sso_client/models.py
+++ innvent_sso_client/models.py
@@ -5,7 +5,25 @@ from django.conf import settings
 from django.db import models
 
 
+class SSOUserTokenManager(models.Manager):
+
+    def create_or_update(self, user, token, expiration_datetime):
+        qs = self.get_query_set()
+
+        try:
+            user_token = qs.get(user=user)
+        except self.model.DoesNotExist:
+            user_token = SSOUserToken(user=user)
+
+        user_token.token = token
+        user_token.expiration_datetime = expiration_datetime
+        user_token.save()
+
+        return user_token
+
+
 class SSOUserToken(models.Model):
+    objects = SSOUserTokenManager()
 
     token = models.CharField(max_length=32, unique=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
