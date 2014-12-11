34648fcc104ec6bc17257c784b30ae24f6eac491
Gerardo Soares <gerardonsoares@gmail.com>
2014-12-11 17:49:31 -0200

    Refatora forma de construir url.

--- innvent_sso_client/tests/test_middlewares.py
+++ innvent_sso_client/tests/test_middlewares.py
@@ -11,28 +11,43 @@ from ..middlewares import SSOMiddleware
 class SSOMiddlewareTestCase(TestCase):
 
     def setUp(self):
-        self.url = 'http://testserver/sso-required/?data=eyJ1c2VyIjp7ImxvZ2luI'\
-        'joidGVzdCIsImVtYWlsIjoidGVzdEBleGFtcGxl%0ALmNvbSIsImZpcnN0X25hbWUiOiJ'\
-        'UZXN0IiwibGFzdF9uYW1lIjoiVXNlciJ9%0ALCJ0b2tlbiI6ImIwYWQ1ZTMwNWFhOGExM'\
-        'GViZTk1NTUyMGI1YmFiOTA3In0%3D%0A'
+        self.data = {
+            'user': {
+                'login': 'test',
+                'email': 'test@example.com',
+                'fist_name': 'Test',
+                'last_name': 'User',
+            },
+            'token': 'b0ad5e305aa8a10ebe955520b5bab907'
+        }
 
         self.factory = RequestFactory()
         self.middleware = SSOMiddleware()
 
+    def __get_url(self, data=None):
+        url = 'http://testserver/foo/bar/'
+
+        if data:
+            json_data = json.dumps(data)
+            b64_data = base64.urlsafe_b64encode(json_data)
+
+            url = '{0}?data={1}'.format(url, b64_data)
+
+        return url
+
     def test_middleware_requires_authentication_middleware(self):
-        request = self.factory.get(self.url)
+        request = self.factory.get(self.__get_url(self.data))
 
         self.assertRaises(
             ImproperlyConfigured, self.middleware.process_request, request
         )
 
     def test_parse_data_returns_user_data_plus_token(self):
-        request = self.factory.get(self.url)
-
-        expected_data = json.loads(base64.b64decode(request.GET['data']))
+        request = self.factory.get(self.__get_url(self.data))
 
         token, user_data = self.middleware.extract_user_data(request)
 
-        self.assertEqual(expected_data['token'], token)
-        self.assertEqual(expected_data['user'], user_data)
+        self.assertEqual(self.data['token'], token)
+        self.assertEqual(self.data['user'], user_data)
+
 
