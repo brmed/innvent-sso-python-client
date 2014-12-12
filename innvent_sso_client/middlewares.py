e41ef43b64cc09144230a980dee05877387897fc
Gerardo Soares <gerardonsoares@gmail.com>
2014-12-12 16:26:36 -0200

    Refatora middleware.

--- innvent_sso_client/middlewares.py
+++ innvent_sso_client/middlewares.py
@@ -20,19 +20,21 @@ class SSOMiddleware(object):
                 " before the SSOAuthenticationMiddleware class."
             )
 
-        session_token = request.session.get('SSO_TOKEN')
-        session_token_expiration = request.session.get('SSO_TOKEN_EXPIRATION')
-
-        if not (session_token and session_token_expiration):
-            # não existe token ou expiração na sessão
+        if self.logout_expired_token_user(request):
             return
 
-        token, user_data = self.extract_user_data(request)
+        try:
+            session_token, token_expiration = self.get_session_data(request)
+        except TypeError:
+            return
+
+        try:
+            token, user_data = self.extract_user_data(request)
+        except TypeError:
+            return
 
         if token != session_token:
             return
-
-        token_expiration = parse(session_token_expiration)
 
         user = authenticate(
             token=token,
@@ -43,17 +45,40 @@ class SSOMiddleware(object):
             last_name=user_data['last_name'],
         )
 
-        if user.ssousertoken.has_expired:
-            logout(request)
+        if not user:
             return
 
-        if user:
+        if not self.logout_expired_token_user(request, user):
             request.user = user
             login(request, user)
 
     def extract_user_data(self, request):
         b64_data = request.GET.get('data')
+        if not b64_data:
+            # não existe data na querystring
+            return None
+
         json_data = base64.b64decode(b64_data)
         data = json.loads(json_data)
 
         return data['token'], data['user']
+
+    def get_session_data(self, request):
+        session_token = request.session.get('SSO_TOKEN')
+        session_token_expiration = request.session.get('SSO_TOKEN_EXPIRATION')
+
+        if not (session_token and session_token_expiration):
+            # não existe token ou expiração na sessão
+            return None
+
+        token_expiration = parse(session_token_expiration, ignoretz=True)
+
+        return session_token, token_expiration
+
+    def logout_expired_token_user(self, request, user=None):
+        user = user or request.user
+
+        if user.is_authenticated() and user.ssousertoken.has_expired:
+            logout(request)
+            return True
+
