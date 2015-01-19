6922f4ecc16b61df50f8be2dedda88b06171cf7c
Gerardo Soares <gerardonsoares@gmail.com>
2015-01-19 12:24:14 -0200

    Adiciona migração para o django 1.5

--- innvent_sso_client/migrations/0001_initial.py
+++ innvent_sso_client/migrations/0001_initial.py
@@ -1,28 +1,110 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
-from django.conf import settings
+from django.db import models
+
+import django
+if django.get_version().startswith('1.5'):
+    # Migração para o django 1.5 usando south
+
+    import datetime
+    from south.db import db
+    from south.v2 import SchemaMigration
+
+    class Migration(SchemaMigration):
+
+        def forwards(self, orm):
+
+            # Adding model 'SSOUserToken'
+            db.create_table(u'innvent_sso_client_ssousertoken', (
+                (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+                ('token', self.gf('django.db.models.fields.CharField')(unique=True, max_length=32)),
+                ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+                ('expiration_datetime', self.gf('django.db.models.fields.DateTimeField')()),
+                ('last_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ))
+            db.send_create_signal(u'innvent_sso_client', ['SSOUserToken'])
 
 
-class Migration(migrations.Migration):
+        def backwards(self, orm):
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
+            # Deleting model 'SSOUserToken'
+            db.delete_table(u'innvent_sso_client_ssousertoken')
 
-    operations = [
-        migrations.CreateModel(
-            name='SSOUserToken',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('token', models.CharField(unique=True, max_length=32)),
-                ('expiration_datetime', models.DateTimeField()),
-                ('last_modified', models.DateTimeField(auto_now=True)),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
+
+        models = {
+            u'auth.group': {
+                'Meta': {'object_name': 'Group'},
+                u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+                'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+                'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
             },
-            bases=(models.Model,),
-        ),
-    ]
+            u'auth.permission': {
+                'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+                'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+                'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+                u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+                'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+            },
+            u'auth.user': {
+                'Meta': {'object_name': 'User'},
+                'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2015, 1, 19, 12, 19, 8, 593639)'}),
+                'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+                'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+                'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+                u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+                'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+                'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+                'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+                'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2015, 1, 19, 12, 19, 8, 593147)'}),
+                'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+                'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+                'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+                'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+            },
+            u'contenttypes.contenttype': {
+                'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+                'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+                u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+                'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+                'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            },
+            u'innvent_sso_client.ssousertoken': {
+                'Meta': {'object_name': 'SSOUserToken'},
+                'expiration_datetime': ('django.db.models.fields.DateTimeField', [], {}),
+                u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+                'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+                'token': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'}),
+                'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+            }
+        }
+
+        complete_apps = ['innvent_sso_client']
+
+else:
+    # Migração para o django 1.7
+    from django.db import migrations
+    from django.conf import settings
+
+
+    class Migration(migrations.Migration):
+
+        dependencies = [
+            migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ]
+
+        operations = [
+            migrations.CreateModel(
+                name='SSOUserToken',
+                fields=[
+                    ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                    ('token', models.CharField(unique=True, max_length=32)),
+                    ('expiration_datetime', models.DateTimeField()),
+                    ('last_modified', models.DateTimeField(auto_now=True)),
+                    ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+                ],
+                options={
+                },
+                bases=(models.Model,),
+            ),
+        ]
