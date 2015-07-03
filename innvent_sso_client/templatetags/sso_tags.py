9cda69ca84c6108bffeb8059a3276a5619a8486d
Gerardo Soares <gerardonsoares@gmail.com>
2015-07-03 08:17:31 -0300

    Conserta a templatetag.

--- innvent_sso_client/templatetags/sso_tags.py
+++ innvent_sso_client/templatetags/sso_tags.py
@@ -5,7 +5,7 @@ from django import template
 register = template.Library()
 
 def is_authenticated(request):
-    authenticated = request.user.is_authenticated()
+    authenticated = hasattr(request, 'user') and request.user.is_authenticated()
     app_permission =  request.session.get('SSO_APPLICATION_PERMISSION', True)
 
     return authenticated and app_permission
