057319461991537876ac4e89342f1c10276e4eea
Bernardo Fontes <bernardoxhc@gmail.com>
2015-08-07 16:21:11 -0300

    Corrige template tag para pular autenticação por app

--- innvent_sso_client/templatetags/sso_tags.py
+++ innvent_sso_client/templatetags/sso_tags.py
@@ -1,12 +1,21 @@
 # coding: utf-8
 from django import template
+from django.conf import settings
 
 
 register = template.Library()
 
+
 def is_authenticated(request):
     authenticated = hasattr(request, 'user') and request.user.is_authenticated()
-    app_permission =  request.session.get('SSO_APPLICATION_PERMISSION', True)
+    app_permission = True
+
+    check_application_permission = getattr(
+        settings, 'SSO_CHECK_APPLICATION_PERMISSION', True
+    )
+
+    if check_application_permission:
+        app_permission = request.session.get('SSO_APPLICATION_PERMISSION', True)
 
     return authenticated and app_permission
 
--- innvent_sso_client/tests/test_tags.py
+++ innvent_sso_client/tests/test_tags.py
@@ -3,6 +3,7 @@ from mock import patch, Mock
 
 from django.contrib.auth.models import AnonymousUser
 from django.test import RequestFactory
+from django.test.utils import override_settings
 
 from .testtools import TestCase
 from ..templatetags.sso_tags import is_authenticated
@@ -28,4 +29,10 @@ class IsAuthenticatedFilterTestCase(TestCase):
     @patch.object(AnonymousUser, 'is_authenticated', Mock(return_value=True))
     def test_should_return_true_only_if_both_SSO_APPLICATION_PERMISSION_and_user_is_authenticated(self):
         self.request.session['SSO_APPLICATION_PERMISSION'] = True
+        self.assertTrue(is_authenticated(self.request))
+
+    @patch.object(AnonymousUser, 'is_authenticated', Mock(return_value=True))
+    @override_settings(SSO_CHECK_APPLICATION_PERMISSION = False)
+    def test_should_return_true_if_user_is_authenticated_and_does_not_need_to_check_for_app_permission(self):
+        self.request.session['SSO_APPLICATION_PERMISSION'] = False
         self.assertTrue(is_authenticated(self.request))
