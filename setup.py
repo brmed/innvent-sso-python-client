fe4a62832c52e1905e442e6b0f16b8c65148383b
Saulo Harã <saulohara@gmail.com>
2017-12-20 18:53:23 -0200

    Atualizando verão da lib python-dateutil

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     url='https://github.com/innvent/innvent-sso-python-client',
     packages=find_packages(),
     test_suite='innvent_sso_client.tests',
-    install_requires=['Django>=1.5.3', 'requests>=2.4.1', 'python-dateutil==1.5'],
+    install_requires=['Django>=1.5.3', 'requests>=2.4.1', 'python-dateutil==2.6.1'],
     tests_require=['tox>=1.6.1', 'virtualenv>=1.11.2'],
     cmdclass = {'test': Tox},
 )
