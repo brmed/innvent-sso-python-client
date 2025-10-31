679511181d9c0b30c45e615379eda804fa50330f
Leandro Josino <leandro.j.c@outlook.com>
2025-10-31 01:51:21 -0300

    fix: reading interface param from request session

--- innvent_sso_client/decorators.py
+++ innvent_sso_client/decorators.py
@@ -42,8 +42,8 @@ def sso_required(view_func):
         qs['redirect_url'] = redirect_path
         qs['token'] = token_dict['token']
 
-        if request.COOKIES.get('display_custom_logo'):
-            qs['interface'] = request.COOKIES.get('logo_key')
+        if request.session.get('display_custom_logo', ''):
+            qs['interface'] = request.session.get('logo_key', '')
 
         redirect_url = '{0}?{1}'.format(
             sso_hostname('/authorize'), qs.urlencode(safe='/')
