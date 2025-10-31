749a38e184ff7c348d985b6f5bc27501e748e88b
Leandro Josino <leandro.j.c@outlook.com>
2025-10-31 00:57:48 -0300

    fix: presentation keys in session

--- innvent_sso_client/decorators.py
+++ innvent_sso_client/decorators.py
@@ -52,13 +52,10 @@ def sso_required(view_func):
         request.session["SSO_TOKEN"] = token_dict['token']
         request.session["SSO_TOKEN_EXPIRATION"] = token_dict['expires_at'].isoformat()
 
-        if 'HTTP_COOKIE' in request.META:
-            cookie = SimpleCookie(request.META.get('HTTP_COOKIE', '') or '')
-            if 'display_custom_logo' in cookie:
-                del cookie['display_custom_logo']
-            if 'logo_key' in cookie:
-                del cookie['logo_key']
-            request.META['HTTP_COOKIE'] = '; '.join(['%s=%s' % (k, v.value) for k, v in cookie.items()])
+        if 'display_custom_logo' in request.session:
+            del request.session['display_custom_logo']
+        if 'logo_key' in request.session:
+            del request.session['logo_key']
 
         return HttpResponseRedirect(redirect_url)
 
